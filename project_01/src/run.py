609a96668d8eccd35cb6bd13b4c9624ba4438c8e
Connor <70863090+Connor-Kelly@users.noreply.github.com>
2023-04-03 22:27:03 -0500

    organized files

--- project_01/src/run.py
+++ project_01/src/run.py
@@ -1,5 +1,33 @@
-import spi_screen as Screen
+import encoder
 
-display = Screen.SPI_Display()
 
+
+# ---
+
+from event_button import EventButton
+import Adafruit_BBIO.GPIO as GPIO
+from cursor import Cursor
+from spi_screen import SPI_Display as Display
+
+
+def go_up(self):
+    print("go up")
+    d.display_image(c.cursor_up())
+
+
+
+def go_dn(self):
+    print("go down")
+    d.display_image(c.cursor_dn())
+
+
+
+pinBtnUp = "P2_22"
+pinBtnDn = "P2_24"
+
+c = Cursor()
+d = Display()
+
+BtnUp = EventButton(pinBtnUp, GPIO.FALLING, go_up)
+BtnDn = EventButton(pinBtnDn, GPIO.FALLING, go_dn)
 
